c08574aac7cd324184d2569b8e2a4b57f2c5ddbf
Robert Rinaldo <rinaldo646@gmail.com>
2021-03-28 21:17:23 -0400

    rewrite type system

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,7 +1,13 @@
-// This file is generated. Do not edit. See scripts/typegen.js
+export type GetKey = (map: { [key: string]: any }) => string | undefined
+export type GetKeys = (map: { [key: string]: any }) => string[]
+export type GetIndex = (arr: any[]) => number | undefined
+export type GetIndexes = (arr: any[]) => number[]
 
-export type NumOrGetter = number | ((arr: any[]) => number | undefined)
-export type NumsOrGetter = number[] | ((arr: any[]) => number[])
-export type StrGetter = (map: { [key: string]: any }) => string
-export type StrsOrGetter = string[] | ((map: { [key: string]: any }) => string[])
-export type ValueOf<T> = T[keyof T]
+// PathItem in docs
+export type Key = string | number | string[] | number[] | GetKey | GetIndex | GetKeys | GetIndexes
+
+// PathHelper
+export type PH<K extends Key, T> = K extends string ? { [key in K]?: T }
+    : K extends number | number[] | GetIndex | GetIndexes ? T[]
+    : K extends string[] | GetKey | GetKeys ? { [key: string]: T }
+    : never
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-// This file is generated. Do not edit. See scripts/typegen.js
-
 export * from "./common"
 export * from "./get"
 export * from "./set"
